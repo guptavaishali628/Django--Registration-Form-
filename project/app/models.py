5e91f8c5544d34c98fc5da0ab20d46685f014605
vaishali <guptavaishali628@gmail.com>
2025-12-03 18:02:32 +0530

    Create database table with data in sqlite3

--- project/app/models.py
+++ project/app/models.py
@@ -5,10 +5,18 @@ from django.db import models
 # inherite model from models
 # mandatory to define max_length 
 class Student(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField(max_length=50)
+    Name=models.CharField(max_length=50)
+    Email=models.EmailField(max_length=50)
     Contact=models.IntegerField(max_length=50)
-    details=models.CharField(max_length=50)
-    image=models.ImageField(upload_to='image/')
+    Details=models.CharField(max_length=50)
     #  install pillow from python lib for image store
+    Image=models.ImageField(upload_to='image/')
+    
+    # this method used for string type data print on the html page data.html
+    # def __str__(self):
+    #     return self.Name +" "+ self.Email
+    
+    # this method used for int type data print on the html page data.html by doing typecasting
+    def __str__(self):
+        return str(self.Contact)
     
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# to axis the Student from models.py file 
+from .models import Student
 
 # Create your views here.
 def landing(req):
@@ -13,9 +15,19 @@ def registerData(req):
     print(req.GET)
     print(req.FILES)
     # printing data from dictionary
+    # varibales like name , email etc take from name attribute from register.html act as key
     n=req.POST.get('name')  
     e=req.POST.get('email')
     c=req.POST.get('contact')
     d=req.POST.get('details')
-    image=req.FILES.get('image')
-    print(n,e,c,d,image)
+    i=req.FILES.get('image')
+    print(n,e,c,d,i)
+
+    # create query for storing data into the sqlite3 when we register 
+    Student.objects.create(Name=n, Email=e, Contact=c, Details=d, Image=i)
+    data = Student.objects.all()
+    # printing database data on terminal
+    # print(data)
+
+    # printing the databse data on html page
+    return render(req,'data.html',{'key':data})
